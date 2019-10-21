51b95e1333795a26d7958c6b94f35ea881f08173
Philippe Cochin <philippe.cochin@gmail.com>
2019-10-21 10:35:20 +0800

    Added levels

--- src/game/levels/index.ts
+++ src/game/levels/index.ts
@@ -18,28 +18,58 @@ import level17 from './level17.json';
 import level18 from './level18.json';
 import level19 from './level19.json';
 import level20 from './level20.json';
+import level21 from './level21.json';
+import level22 from './level22.json';
+import level23 from './level23.json';
+import level24 from './level24.json';
+import level25 from './level25.json';
+import level26 from './level26.json';
+import level27 from './level27.json';
+import level28 from './level28.json';
+import level29 from './level29.json';
+import level30 from './level30.json';
+import level31 from './level31.json';
+import level32 from './level32.json';
+import level33 from './level33.json';
+import level34 from './level34.json';
 import { ILevelList } from '@/types';
 
-export { level1 };
-export { level2 };
-export { level3 };
-export { level4 };
-export { level5 };
-export { level6 };
-export { level7 };
-export { level8 };
-export { level9 };
-export { level10 };
-export { level11 };
-export { level12 };
-export { level13 };
-export { level14 };
-export { level15 };
-export { level16 };
-export { level17 };
-export { level18 };
-export { level19 };
-export { level20 };
+export {
+	level1,
+	level2,
+	level3,
+	level4,
+	level5,
+	level6,
+	level7,
+	level8,
+	level9,
+	level10,
+	level11,
+	level12,
+	level13,
+	level14,
+	level15,
+	level16,
+	level17,
+	level18,
+	level19,
+	level20,
+	level21,
+	level22,
+	level23,
+	level24,
+	level25,
+	level26,
+	level27,
+	level28,
+	level29,
+	level30,
+	level31,
+	level32,
+	level33,
+	level34
+};
 
 const levels: ILevelList = {
 	level1,
@@ -61,7 +91,21 @@ const levels: ILevelList = {
 	level17,
 	level18,
 	level19,
-	level20
+	level20,
+	level21,
+	level22,
+	level23,
+	level24,
+	level25,
+	level26,
+	level27,
+	level28,
+	level29,
+	level30,
+	level31,
+	level32,
+	level33,
+	level34
 };
 
 export default levels;
