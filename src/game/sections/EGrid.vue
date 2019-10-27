29fea75ba40ede52a376dce539ac6155afc5f8b2
Philippe Cochin <philippe.cochin@gmail.com>
2019-10-27 12:58:12 +0800

    Self-contained Egrids

---
+++ src/game/sections/EGrid.vue
@@ -0,0 +1,260 @@
+<template>
+  <svg class="grid" :width="totalWidth" :height="totalHeight" ref="grid">
+    <!-- <svg class="grid" :width="totalWidth" :height="totalHeight"> -->
+    <!-- DOTS -->
+    <g v-for="(row, y) in level.grid.rows + 1" :key="y">
+      <g v-for="(column, x) in level.grid.cols + 1" :key="x">
+        <circle :cx="x * tileSize" :cy="y * tileSize" r="1" fill="#edeaf4" />
+      </g>
+    </g>
+
+    <!-- LASER PATH -->
+    <g
+      v-for="(laser, index) in individualLaserPath"
+      :key="'laser' + index"
+      :v-if="individualLaserPath.length > 0"
+      class="lasers"
+    >
+      <path :d="laser" stroke-dasharray="8 8" fill="transparent" stroke="red" stroke-width="3" />
+    </g>
+
+    <!-- CELLS -->
+    <QCell
+      v-for="(cell, i) in level.grid.cells"
+      :key="'cell' + i"
+      :cell="cell"
+      :tileSize="tileSize"
+      @click.native="rotate(cell)"
+    />
+
+    <!-- PHOTONS -->
+    <g
+      v-for="(particle, index) in frame.quantum"
+      :key="'particle' + index"
+      :v-if="frame.quantum.length > 0"
+      :style="computeParticleStyle(particle)"
+      class="photons"
+    >
+      <photon
+        name
+        :intensity="particle.intensity"
+        :are="particle.a.re"
+        :aim="particle.a.im"
+        :bre="particle.b.re"
+        :bim="particle.b.im"
+        :width="64"
+        :height="64"
+        :margin="0"
+        :display-magnetic="true"
+        :display-electric="false"
+        :display-gaussian="false"
+        :sigma="0.25"
+      />
+    </g>
+  </svg>
+</template>
+
+<script lang="ts">
+import cloneDeep from 'lodash.clonedeep';
+import { Vue, Prop, Component } from 'vue-property-decorator';
+import {
+  Level,
+  Particle,
+  Frame,
+  Grid,
+  Cell,
+  ParticleInterface,
+  CellInterface,
+  Coord,
+  LevelInterface
+} from 'quantumweasel';
+import Photon from '../Photon.vue';
+import QCell from '../QCell.vue';
+
+@Component({
+  components: {
+    Photon,
+    QCell
+  }
+})
+export default class EGrid extends Vue {
+  @Prop({ default: {} }) readonly levelObj!: LevelInterface;
+  @Prop({ default: 4 }) readonly step!: number;
+  tileSize: number = 64;
+  level: Level = Level.importLevel(this.levelObj);
+  frame: Frame = new Frame(this.level);
+  frameNumber: number = 0;
+  frames: Frame[] = [];
+
+  $refs!: {
+    grid: HTMLElement;
+  };
+
+  created() {
+    for (let i = 0; i < this.step; i++) {
+      this.frame = this.frame.next();
+    }
+    window.addEventListener('resize', this.assessTileSize);
+  }
+
+  mounted() {
+    this.assessTileSize();
+  }
+
+  createNextFrame() {
+    const lastFrameCopy = cloneDeep(this.lastFrame);
+    const nextFrame = lastFrameCopy.next();
+    this.frames.push(nextFrame);
+  }
+
+  get activeFrame() {
+    return this.frames[this.frameNumber];
+  }
+
+  assessTileSize() {
+    const currentWidth = this.$refs.grid.getBoundingClientRect().width;
+    // this.tileSize = currentWidth / this.grid.cols;
+    this.tileSize = 64;
+  }
+
+  get lasers(): ParticleInterface[] {
+    return this.level.grid.computePaths();
+  }
+
+  get totalWidth(): number {
+    return this.level.grid.cols * this.tileSize;
+  }
+  get totalHeight(): number {
+    return this.level.grid.rows * this.tileSize;
+  }
+
+  computeParticleStyle(particle: ParticleInterface): {} {
+    const originX = this.centerCoord(particle.coord.x);
+    const originY = this.centerCoord(particle.coord.y);
+    return {
+      'transform-origin': `${originX}px ${originY}px`,
+      transform: `
+				rotate(${particle.direction}deg)
+				translate(${particle.coord.x * this.tileSize}px, ${particle.coord.y * this.tileSize}px)`
+    };
+  }
+
+  /**
+   * Compute the cell center at a specific coordinate for grid dots
+   * @returns x, y pixel coordinates
+   */
+  centerCoord(val: number): number {
+    return (val + 0.5) * this.tileSize;
+  }
+
+  /**
+   * Cell rotation
+   */
+  rotate(cell: Cell) {
+    cell.rotate();
+    console.log(cell.toString());
+    this.level.grid.set(cell);
+  }
+
+  /**
+   * Create laser path through the lasers points
+   * @returns SVG laser path
+   */
+  laserPath(): string {
+    let pathStr = '';
+    if (this.lasers.length > 0) {
+      const originX = this.centerCoord(this.lasers[0].coord.x);
+      const originY = this.centerCoord(this.lasers[0].coord.y);
+      pathStr += `M ${originX} ${originY} `;
+      this.lasers.forEach((laser: any) => {
+        const x = this.centerCoord(laser.coord.x);
+        const y = this.centerCoord(laser.coord.y);
+        pathStr += ` L ${x} ${y} `;
+      });
+      pathStr += ' ';
+    }
+    return pathStr;
+  }
+
+  get individualLaserPath(): string[] {
+    const pathsStr: string[] = [];
+    if (this.lasers.length > 0) {
+      this.lasers.forEach((laser: any) => {
+        let pathStr = '';
+        const originX = this.centerCoord(laser.coord.x);
+        const originY = this.centerCoord(laser.coord.y);
+        pathStr += `M ${originX} ${originY} `;
+        switch (laser.direction) {
+          case 0:
+            pathStr += ` H ${this.centerCoord(laser.coord.x + 1)}`;
+            break;
+          case 90:
+            pathStr += ` V ${this.centerCoord(laser.coord.y - 1)}`;
+            break;
+          case 180:
+            pathStr += ` H ${this.centerCoord(laser.coord.x - 1)}`;
+            break;
+          case 270:
+            pathStr += ` V ${this.centerCoord(laser.coord.y + 1)}`;
+            break;
+          default:
+            throw new Error(`Laser has wrong direction: ${laser.direction}°`);
+        }
+        pathsStr.push(pathStr);
+      });
+    }
+    return pathsStr;
+  }
+
+  /**
+   * Create laser path through the lasers points
+   * @returns SVG laser path
+   */
+  photonPath(): string {
+    let pathStr = '';
+    if (this.frame.quantum.length > 0) {
+      const originX = this.centerCoord(this.frame.quantum[0].coord.x);
+      const originY = this.centerCoord(this.frame.quantum[0].coord.y);
+      pathStr += `M ${originX} ${originY} `;
+      this.lasers.forEach((laser: any) => {
+        const x = this.centerCoord(laser.coord.x);
+        const y = this.centerCoord(laser.coord.y);
+        pathStr += ` L ${x} ${y} `;
+      });
+    }
+    return pathStr;
+  }
+
+  // HELPING FUNCTIONS
+  element(y: number, x: number): CellInterface {
+    const cells = this.level.grid.cells.filter(
+      (cell: Cell) => cell.coord.x === x && cell.coord.y === y
+    );
+    if (cells.length > 0) {
+      return cells[0].exportCell();
+    }
+    return {
+      coord: { x, y },
+      element: 'Void',
+      rotation: 0,
+      frozen: false
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.laserPath {
+  stroke-dasharray: 8;
+  animation-name: dash;
+  animation-duration: 4s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  animation-direction: reverse;
+}
+@keyframes dash {
+  to {
+    stroke-dashoffset: 64;
+  }
+}
+</style>
--- src/info/Entry.vue
+++ src/info/Entry.vue
@@ -1,24 +1,26 @@
 <template>
-	<div v-if="entry" class="entry">
-		<router-link to="/info">
-			<q-button type="basic">ENCYCLOPEDIA</q-button>
-		</router-link>
-		<article>
-			<h1 class="title">{{ entry.title.toUpperCase() }}</h1>
-			<h2 class="short">{{ entry.short }}</h2>
-			<div class="placeholder">
-				<div class="board">
-					<img src="@/assets/test_board_en.svg" alt="placeholder" />
-				</div>
-			</div>
-			<entry-section
-				v-for="(section, index) in entry.sections"
-				:key="section.title"
-				:section="section"
-				:should-be-open-on-init="index === 0"
-			/>
-		</article>
-	</div>
+  <div v-if="entry" class="entry">
+    <router-link to="/info">
+      <q-button type="basic">ENCYCLOPEDIA</q-button>
+    </router-link>
+    <article>
+      <h1 class="title">{{ entry.title.toUpperCase() }}</h1>
+      <h2 class="short">{{ entry.short }}</h2>
+
+      <div class="boards">
+        <div v-for="(level, i) in levels" :key="'level' + i">
+          <Egrid :levelObj="level.levelObj" :step="level.step" class="board" />
+        </div>
+      </div>
+
+      <entry-section
+        v-for="(section, index) in entry.sections"
+        :key="section.title"
+        :section="section"
+        :should-be-open-on-init="index === 0"
+      />
+    </article>
+  </div>
 </template>
 
 <script lang="ts">
@@ -27,94 +29,108 @@ import EntrySection, { ISection } from './EntrySection.vue';
 import Photon from '../game/Photon.vue';
 import QButton from '../components/QButton.vue';
 import { getEntry } from './entries';
+import { Level } from 'quantumweasel';
+import Egrid from '../game/sections/EGrid.vue';
+import BeamSplitterLevel1 from '../game/levels/encyclopedia/BeamSplitterLevel1.json';
+import BeamSplitterLevel2 from '../game/levels/encyclopedia/BeamSplitterLevel2.json';
 
 interface IEntryList {
-	[index: string]: IEntry;
+  [index: string]: IEntry;
 }
 
 interface IEntry {
-	title: string;
-	short?: string;
-	sections: Array<ISection>;
+  title: string;
+  short?: string;
+  sections: Array<ISection>;
 }
 
 @Component({
-	components: {
-		EntrySection,
-		QButton,
-		Photon
-	}
+  components: {
+    EntrySection,
+    Egrid,
+    QButton,
+    Photon
+  }
 })
 export default class Entry extends Vue {
-	entry: IEntry = {
-		title: '',
-		sections: []
-	};
+  entry: IEntry = {
+    title: '',
+    sections: []
+  };
 
-	created() {
-		this.loadEntry();
-	}
+  levels = [{ levelObj: BeamSplitterLevel1, step: 4 }, { levelObj: BeamSplitterLevel2, step: 3 }];
 
-	@Watch('$route')
-	loadEntry() {
-		if (this.entryURL) {
-			this.entry = getEntry(this.entryURL);
-		}
-		if (!this.entry.title) {
-			this.$router.push({ name: '404' });
-		}
-	}
+  created() {
+    this.loadEntry();
+  }
 
-	get entryURL(): string {
-		return this.$route.params.entry;
-	}
+  @Watch('$route')
+  loadEntry() {
+    if (this.entryURL) {
+      this.entry = getEntry(this.entryURL);
+    }
+    if (!this.entry.title) {
+      this.$router.push({ name: '404' });
+    }
+  }
+
+  get entryURL(): string {
+    return this.$route.params.entry;
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .entry {
-	display: flex;
-	flex-direction: column;
-	justify-content: space-around;
-	align-items: center;
-	& .title {
-		font-size: 2rem;
-		font-weight: bold;
-	}
-	& .short {
-		font-size: 1rem;
-	}
-	& .go-back {
-		font-weight: bold;
-		text-decoration: none;
-		color: white;
-	}
-	& p {
-		line-height: 2em;
-		text-align: left;
-	}
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  & .title {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+  & .short {
+    font-size: 1rem;
+  }
+  & .go-back {
+    font-weight: bold;
+    text-decoration: none;
+    color: white;
+  }
+  & p {
+    line-height: 2em;
+    text-align: left;
+  }
 }
 
 h1 {
-	padding-bottom: 1rem;
-	border-bottom: 1px solid white;
-	text-align: center;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid white;
+  text-align: center;
+}
+
+.board {
+  display: inline;
+	border-style: solid;
+  border-color: grey;
+
 }
 
 .placeholder {
-	padding-top: 2rem;
-	width: 100%;
-	border-bottom: 1px solid #8e819d;
-	& .board {
-		width: 100%;
-		margin: 0 auto 0rem;
-		height: 200px;
-		text-align: center;
-		padding-bottom: 4rem;
-		& span {
-			font-size: 1rem;
-			color: gold;
-		}
-	}
+  padding-top: 2rem;
+  width: 100%;
+  border-bottom: 1px solid #8e819d;
+  & .board {
+    width: 100%;
+    margin: 0 auto 0rem;
+    height: 200px;
+    text-align: center;
+    padding-bottom: 4rem;
+    & span {
+      font-size: 1rem;
+      color: gold;
+    }
+  }
 }
 </style>
