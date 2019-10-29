10fc18ec1320b39afc775cc2e215bc20317f075f
Jakub Strebeyko <strebeyko@gmx.com>
2019-10-29 06:10:31 +0100

    Add levels view

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ body {
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
-	color: #2c3e50;
+	color: white;
 	padding: 0;
 	width: 100%;
 	height: 100%;
