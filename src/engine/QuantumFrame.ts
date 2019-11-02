06044673014621ecf411a2e7c4d775d525fdb877
Piotr Migdał <pmigdal@gmail.com>
2019-11-03 01:53:01 +0800

    feat(engine) created lightweight QuantumSimulation

---
+++ src/engine/QuantumFrame.ts
@@ -0,0 +1,40 @@
+import * as qt from 'quantum-tensors';
+
+import Coord from './Coord';
+import Particle, { Qparticle } from './Particle';
+
+/**
+ * QuantumFrame is essentially a wrapper on Photons from qunatum-tensors.
+ * It hold quantum state at a given time, and nothing more.
+ * All other things are accessible with getters.
+ * @todo Make it immutable? Make photons private or expose?
+ */
+export default class QuantumFrame {
+  readonly photons: qt.Photons;
+
+  constructor(sizeX: number, sizeY: number) {
+    this.photons = new qt.Photons(sizeX, sizeY);
+  }
+
+  get ket(): qt.Vector {
+    return this.photons.vector;
+  }
+
+  get ketString(): string {
+    return this.photons.ketString();
+  }
+
+  get probability(): number {
+    return this.photons.vector.normSquared();
+  }
+
+  /**
+   * @note Cord and Partile will need a serious rewrite
+   */
+  get polarizationSuperpositions(): Particle[] {
+    return this.photons.aggregatePolarization().map((q: Qparticle) => {
+      const coord = new Coord(q.y, q.x);
+      return new Particle(coord, q.direction, 0, 0, q.are, q.aim, q.bre, q.bim);
+    });
+  }
+}
