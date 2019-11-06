524fa28e7cb09b1c22c9b1730c745c3dfdfd294c
Jakub Jan Strebeyko <strebeyko@gmx.com>
2019-11-06 07:36:35 +0100

    Entries from markdown (#58)
    * create an updateEntries script;
    - [x] adjust the EncyclopediaArticle & EncyclopediaArticleSection;
    - [x] mock the markdowns for the components
    
    * lint fixes

--- src/assets/data/entries/index.ts
+++ src/assets/data/entries/index.ts
@@ -1,12 +1,12 @@
 import { EntryListInterface, EntryInterface } from '@/engine/interfaces';
-import entries from './entries.json';
+import entries from './entries-test.json';
 
 export const typedEntries: EntryListInterface = entries;
 
 // used by views/Info to render the left menu:
 export const entriesNameList = Object.keys(typedEntries);
 
-// used by info/Entry
+// used by Encyclopedia Article
 export function getEntry(name: string): EntryInterface {
   return typedEntries[name];
 }
---
+++ src/components/EncyclopediaPage/EncyclopediaLinkList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div slot="left" class="element-list">
+    <h3 class="upper-border">ALL ELEMENTS</h3>
+    <router-link v-for="entry in entryList" :key="entry" :to="`/info/${entry}`">
+      <div>{{ spacedEntry(entry) }}</div>
+    </router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch, Prop } from 'vue-property-decorator';
+
+@Component
+export default class EncyclopediaLinkList extends Vue {
+  @Prop() readonly entryList!: Array<string>;
+
+  spacedEntry(name: string) {
+    const nameCopy = name;
+    return nameCopy.replace(/-/g, ' ');
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.element-list {
+  a {
+    color: white;
+    text-decoration: none;
+    line-height: 200%;
+  }
+}
+</style>
