7441f7fbea3ee9a1282a6afb594b496e35c4be4d
Piotr Migdał <pmigdal@gmail.com>
2019-11-07 22:32:30 +0800

    feat(Ket) ket components data to GameKet + example

--- src/components/GamePage/GameKet.vue
+++ src/components/GamePage/GameKet.vue
@@ -1,8 +1,24 @@
 <template>
   <div ref="wrapper" class="simulation-frame-kets">
-    <span>
-      {{ frame.photons.ketString() }}
-    </span>
+    <div>
+      Ket (old): {{ frame.photons.ketString() }}
+    </div>
+    <div>
+      <span v-for="(ketComponent, index) in ketComponents" :key="`ket-component-${index}`" class="ket-component">
+        <span>
+          + {{ renderComplex(ketComponent.amplitude) }}
+        </span>
+        <span v-for="(particleCoord, index) in ketComponent.particleCoords" :key="`ket-component-${index}`" class="ket-component">
+          | {{ particleCoord.x }},{{ particleCoord.y }} {{ renderDir(particleCoord.dir) }} {{ renderPol(particleCoord.pol) }}  ⟩
+        </span>
+      </span>
+    </div>
+    <div>
+      Absorptions: 
+      <span v-for="(absorption, index) in absorptions" :key="`absorption-${index}`" class="absorption">
+        {{ toPercent(absorption.probability)}}% at ({{ absorption.x }}, {{ absorption.y }})
+      </span>
+    </div>
   </div>
 </template>
 
@@ -13,6 +29,7 @@ import Particle from '@/engine/Particle';
 import AppPhoton from '@/components/AppPhoton.vue';
 import AppButton from '@/components/AppButton.vue';
 import QuantumFrame from '@/engine/QuantumFrame';
+import { Complex } from 'quantum-tensors'; 
 
 @Component({
   components: {
@@ -22,6 +39,33 @@ import QuantumFrame from '@/engine/QuantumFrame';
 })
 export default class GameKet extends Vue {
   @Prop() readonly frame!: QuantumFrame;
+
+  toPercent(x: number, precision = 1): string {
+    return (100 * x).toFixed(precision)
+  }
+
+  renderComplex(z: Complex, precision = 2) {
+    return `${z.r.toFixed(precision)} exp(i${z.phi.toFixed(precision)})`;
+  }
+
+  renderDir(dir: number) {
+    return ['⤑', '⇡', '⇠', '⇣'][dir];
+    // or return ["⟷", "↕︎"][pol];
+  }
+
+  renderPol(pol: number) {
+    return ['H', 'V'][pol];
+    // or return ["⟷", "↕︎"][pol];
+  }
+
+  get absorptions() {
+    return this.frame.absorptions;
+  }
+
+  get ketComponents() {
+    return this.frame.ketComponents;
+  }
+
 }
 </script>
 
