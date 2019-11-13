bcf64e50211c8d3f3fcdfbdd2e7c9d37d0db327f
Klem Jankiewicz <k.jankiewicz@gmail.com>
2019-11-13 16:47:49 +0800

    Homepage Authors

--- src/components/HomePage/index.vue
+++ src/components/HomePage/index.vue
@@ -21,35 +21,27 @@
         <a href="https://medium.com/quantum-photons" target="_blank">Medium</a>.
       </h1>
       <p>
+        Quantum Game is currently being developed by dr
+        <a href="https://p.migdal.pl/" target="_blank">Piotr Migdał</a>,
+        <a href="https://fr.linkedin.com/in/philippe-cochin-b4978231" target="_blank"
+          >Philippe Cochin</a
+        >, <a href="https://maperseguirvirtute.wordpress.com/" target="_blank">Chiara Decaroli</a>,
+        <a href="http://jankiewiczstudio.com/" target="_blank">Klem Jankiewicz</a>,
+        <a href="https://www.linkedin.com/in/strebeyko/" target="_blank">Kuba Strebeyko</a>.
+      </p>
+      <p>
         This version of the game was funded by the
         <a href="https://www.quantumlah.org/" target="_blank">Center for Quantum Technologies</a>
         of the National University of Singapore.
       </p>
       <img src="@/assets/CQT_NUS_Logo_mono.svg" alt="CQT logo" class="CQTlogo" />
-      <!-- newsletter -->
-      <!-- <div id="content" class="newsletter">
-        <h1>Sign up for our newsletter now!</h1>
-        <input type="email" placeholder="Email" />
-        <q-button type="special" href="#">SIGN UP</q-button>
-      </div>-->
-
+      <p>
+        You can play the old version
+        <a href="http://play.quantumgame.io/" target="_blank">here</a>.
+      </p>
       <div class="image">
         <img src="@/assets/Rock.svg" alt="Rock" />
       </div>
-      <!-- <div class="blog">
-        <h2>Wave optics and quantum mechanics as a game</h2>
-        <p>
-          NEWS: We develop Quantum Game v 2.0 at CQT, Singapore. See updates at Medium or Twitter.
-          We will release it late 2019. for fun! Learn quantum mechanics as a side effect. It’s a
-          puzzle game, heavily inspired by The Incredible Machine and Chromatron. All you need is a
-          browser (we recommend recent version of Firefox or Chrome), you don’t need to install
-          anything. It’s open source - so not only you can play it for free, but also lookup the
-          code. Motivation behind the game is in Quantum mechanics for high-school students blog
-          post. It was covered on The Quantum Times (top pick in education for gamifying quantum
-          theory), Motherboard, Scott Aaronson, Cory Doctorow, Hacker News (actually, twice). For
-          more games like this one, see: Science-based games collaborative list.
-        </p>
-      </div>-->
     </div>
   </div>
 </template>
@@ -122,7 +114,7 @@ p {
   align-items: center;
 }
 .image {
-  width: 40%;
+  width: 30%;
   padding-bottom: 10px;
   min-width: 200px;
 }
