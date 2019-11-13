a95ecc377c4f2a1f83f588a4b3c5369a0a0060af
Piotr Gajcy <piotrgajcy@users.noreply.github.com>
2019-11-13 10:57:15 +0100

    Firebase saved levels (#88)
    * resolved changes
    
    * change level data after rebase
    
    * Added subpage for saved levels

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -18,6 +18,8 @@ const userStore: StoreOptions<UserState> = {
       }
     },
     progressArr: [],
+    savedLevelsList: [],
+    publicLevels: [],
     error: null
   },
   getters: {
@@ -29,6 +31,12 @@ const userStore: StoreOptions<UserState> = {
     },
     progressArr(state) {
       return state.progressArr;
+    },
+    savedLevelsList(state) {
+      return state.savedLevelsList;
+    },
+    publicLevels(state) {
+      return state.publicLevels;
     },
     error(state) {
       return state.error;
@@ -49,6 +57,23 @@ const userStore: StoreOptions<UserState> = {
     },
     SET_PROGRESS(state, payload) {
       state.progressArr = payload;
+    },
+    SET_SAVED_LEVELS_LIST(state, payload) {
+      state.savedLevelsList = payload;
+    },
+    SET_PUBLIC_LEVELS(state, payload){
+      state.publicLevels = payload;
+    },
+    SET_PUBLIC(state, payload) {
+      state.savedLevelsList.find(x => x.id === payload)!.public = true;
+    },
+    SET_PRIVATE(state, payload) {
+      state.savedLevelsList.find(x => x.id === payload)!.public = false;
+    },
+    REMOVE_LEVEL(state, payload) {
+      state.savedLevelsList = state.savedLevelsList.filter(function( obj ) {
+        return obj.id !== payload;
+    });
     }
   },
   actions: {
@@ -62,6 +87,8 @@ const userStore: StoreOptions<UserState> = {
       commit('SET_LOGGED_IN', user !== null);
       if (user) {
         dispatch('GET_PROGRESS');
+        dispatch('FETCH_MY_LEVELS');
+        dispatch('FETCH_PUBLIC_LEVELS');
         commit('SET_ERROR', null);
         commit('SET_USER', {
           displayName: user.displayName,
@@ -170,6 +197,183 @@ const userStore: StoreOptions<UserState> = {
           commit('SET_ERROR', err.message);
         });
       }
+    },
+    SAVE_LEVEL({commit}, level) {
+      if (auth.currentUser) {
+        const customLevel: any = {
+          userId: auth.currentUser.uid,
+          level: {
+            currentLevelID: level.currentLevelID,
+            gameState: level.gameState
+          },
+          public: false,
+          createdAt: firebase.firestore.Timestamp.fromDate(new Date())
+        };
+
+        console.log(level)
+        const dbRef = db.collection('levels');
+
+        dbRef
+          .add(customLevel)
+          .then(data => {
+            console.log('Level saved: ', data);
+            router.replace({ path: `/level/${level.currentLevelID}/${data.id}` });
+          })
+          .catch((err) => {
+            commit('SET_ERROR', err.message);
+          });
+      } else {
+        console.log('You are not logged!')
+      }
+    },
+    UPDATE_LEVEL({commit}, level) {
+      if (auth.currentUser) {
+        const customLevel: any = {
+          userId: auth.currentUser.uid,
+          level: {
+            currentLevelID: level.currentLevelID,
+            gameState: level.gameState
+          },
+          //created for firestore update test
+          lastModifed: firebase.firestore.Timestamp.fromDate(new Date())
+        };
+        const levelSaved = router.currentRoute.params.levelsaved;
+
+        const doc = db.collection('levels').doc(levelSaved);
+
+        doc
+          .update(customLevel)
+          .catch((err) => {
+            commit('SET_ERROR', err.message);
+          });
+      } else {
+        console.log('You are not logged!')
+      }
+    },
+    FETCH_MY_LEVELS({commit}) {
+      if (auth.currentUser) {
+        const myLevels: any = [];
+        const docs = db.collection('levels').where("userId", "==", auth.currentUser.uid);
+
+        docs
+          .get()
+          .then(querySnapshot => {
+            querySnapshot.forEach(function(doc) {
+              myLevels.push({
+                id: doc.id,
+                link: `/level/${doc.data().level.currentLevelID}/${doc.id}`,
+                public: doc.data().public
+              })
+            });  
+          })
+          .then(()=> {
+            commit('SET_SAVED_LEVELS_LIST', myLevels)
+          })
+          .catch((err) => {
+            commit('SET_ERROR', err.message);
+          });
+      } else {
+        console.log('You are not logged!')
+      }
+    },
+    FETCH_PUBLIC_LEVELS({commit}) {
+      if (auth.currentUser) {
+        const publicLevels: any = [];
+        const docs = db.collection('shared_levels');
+
+        docs
+          .get()
+          .then(querySnapshot => {
+            querySnapshot.forEach(function(doc) {
+              publicLevels.push({
+                link: `/levels/shared/${doc.data().level.currentLevelID}/${doc.id}`,
+              })
+            });  
+          })
+          .then(()=> {
+            commit('SET_PUBLIC_LEVELS', publicLevels)
+          })
+          .catch((err) => {
+            commit('SET_ERROR', err.message);
+          });
+      } else {
+        console.log('You are not logged!')
+      }
+    },
+    MAKE_LEVEL_PUBLIC({commit}, levelID) {
+      if (auth.currentUser) {        
+
+        const docs = db.collection('levels').where("userId", "==", auth.currentUser.uid).where(firebase.firestore.FieldPath.documentId(), '==', levelID);
+        const sharedDoc = db.collection('shared_levels');
+
+        docs
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach(function(doc) {
+              if (doc.id === levelID) {
+                doc.ref.update({public: true});
+                const docShared = Object.assign({},{"guid": doc.id },doc.data());
+                sharedDoc
+                  .add(docShared)
+                  .then(()=> {
+                    console.log('data added');
+                    commit('SET_PUBLIC', levelID);
+                  })
+              }
+            });            
+          })
+          .catch((err) => {
+            commit('SET_ERROR', err.message);
+          });
+      } else {
+        console.log('You are not logged!')
+      }
+    },
+    MAKE_LEVEL_PRIVATE({commit}, levelID) {
+      if (auth.currentUser) {
+        const doc = db.collection('levels').doc(levelID);
+        const sharedDoc = db.collection('shared_levels').where("userId", "==", auth.currentUser.uid);
+        
+        doc
+          .update({public: false})
+          .then(() => {
+            sharedDoc
+              .where("guid", '==', levelID)
+              .get()
+              .then(querySnapshot => {
+                querySnapshot.forEach(function(d) {
+                  d.ref.delete();
+                })
+              })
+              .then(() => {
+                console.log('data removed');
+                commit('SET_PRIVATE', levelID);
+              })
+              .catch((err) => {
+                commit('SET_ERROR', err.message);
+              });
+            
+          })
+          .catch((err) => {
+            commit('SET_ERROR', err.message);
+          });
+      } else {
+        console.log('You are not logged!')
+      }
+    },
+    REMOVE_LEVEL({commit, dispatch}, levelID) {
+      const dbRef = db.collection('levels');
+
+      dbRef
+        .doc(levelID)
+        .delete()
+        .then(() => {
+          commit('REMOVE_LEVEL', levelID)
+          console.log('Level removed', levelID);          
+        })
+        .catch((err) => {
+          commit('SET_ERROR', err.message);
+        });
     }
   }
 };
