d79dcd09baf448ab2d7b5072cbfd5fe7e23e88e9
Philippe Cochin <philippe.cochin@gmail.com>
2019-11-14 14:22:37 +0800

    Level refactoring

--- src/assets/data/levels/index.ts
+++ src/assets/data/levels/index.ts
@@ -1,117 +1,43 @@
-import level0 from './level0.json';
-import level1 from './level1.json';
-import level2 from './level2.json';
-import level3 from './level3.json';
-import level4 from './level4.json';
-import level5 from './level5.json';
-import level6 from './level6.json';
-import level7 from './level7.json';
-import level8 from './level8.json';
-import level9 from './level9.json';
-import level10 from './level10.json';
-import level11 from './level11.json';
-import level12 from './level12.json';
-import level13 from './level13.json';
-import level14 from './level14.json';
-import level15 from './level15.json';
-import level16 from './level16.json';
-import level17 from './level17.json';
-import level18 from './level18.json';
-import level19 from './level19.json';
-import level20 from './level20.json';
-import level21 from './level21.json';
-import level22 from './level22.json';
-import level23 from './level23.json';
-import level24 from './level24.json';
-import level25 from './level25.json';
-import level26 from './level26.json';
-import level27 from './level27.json';
-import level28 from './level28.json';
-import level29 from './level29.json';
-import level30 from './level30.json';
-import level31 from './level31.json';
-import level32 from './level32.json';
-import level33 from './level33.json';
-import level34 from './level34.json';
-import level999 from './level999.json';
-import { LevelListInterface } from '@/engine/interfaces';
+import { LevelInterface } from '@/engine/interfaces';
+import introLevels from './adventure/introLevels.json';
+import reflectionLevels from './adventure/reflectionLevels.json';
+import phaseLevels from './adventure/phaseLevels.json';
+import interferenceLevels from './adventure/interferenceLevels.json';
+import polarizationLevels from './adventure/polarizationLevels.json';
+import finalLevels from './adventure/finalLevels.json';
+import sandbox from './sandboxLevel.json';
 
 export {
-  level0,
-  level1,
-  level2,
-  level3,
-  level4,
-  level5,
-  level6,
-  level7,
-  level8,
-  level9,
-  level10,
-  level11,
-  level12,
-  level13,
-  level14,
-  level15,
-  level16,
-  level17,
-  level18,
-  level19,
-  level20,
-  level21,
-  level22,
-  level23,
-  level24,
-  level25,
-  level26,
-  level27,
-  level28,
-  level29,
-  level30,
-  level31,
-  level32,
-  level33,
-  level34,
-  level999
+  sandbox,
+  introLevels,
+  reflectionLevels,
+  phaseLevels,
+  interferenceLevels,
+  polarizationLevels,
+  finalLevels
 };
 
-const levels: LevelListInterface = {
-  level0,
-  level1,
-  level2,
-  level3,
-  level4,
-  level5,
-  level6,
-  level7,
-  level8,
-  level9,
-  level10,
-  level11,
-  level12,
-  level13,
-  level14,
-  level15,
-  level16,
-  level17,
-  level18,
-  level19,
-  level20,
-  level21,
-  level22,
-  level23,
-  level24,
-  level25,
-  level26,
-  level27,
-  level28,
-  level29,
-  level30,
-  level31,
-  level32,
-  level33,
-  level34,
-  level999
-};
+const introGroup: LevelInterface[] = introLevels;
+const reflectionGroup: LevelInterface[] = reflectionLevels;
+const phaseGroup: LevelInterface[] = phaseLevels;
+const interferenceGroup: LevelInterface[] = interferenceLevels;
+const polarizationGroup: LevelInterface[] = polarizationLevels;
+const finalGroup: LevelInterface[] = finalLevels;
+
+const levels: LevelInterface[] = [
+  sandbox,
+  introGroup[0],
+  introGroup[1],
+  reflectionGroup[0],
+  reflectionGroup[1],
+  phaseGroup[0],
+  phaseGroup[1],
+  interferenceGroup[0],
+  interferenceGroup[1],
+  polarizationGroup[0],
+  polarizationGroup[1],
+  finalGroup[0],
+  finalGroup[1]
+];
 
 export default levels;
