c7369ca1cd242d98add9f0499e4ef8312b6f8e63
Klem <klementyna@Klementynas-MacBook-Air.local>
2019-11-29 17:28:37 +0100

    smaller level title arrows

--- src/components/GamePage/index.vue
+++ src/components/GamePage/index.vue
@@ -17,11 +17,11 @@
       <h1 v-if="error" slot="header-middle" class="error">{{ error }}</h1>
       <h1 v-else slot="header-middle" class="title">
         <router-link :to="previousLevel">
-          <img src="@/assets/graphics/icons/previousLevel.svg" alt="Previous Level" width="32" />
+          <img src="@/assets/graphics/icons/previousLevel.svg" alt="Previous Level" width="24" />
         </router-link>
         {{ level.id + ' - ' + level.name.toUpperCase() }}
         <router-link :to="nextLevel">
-          <img src="@/assets/graphics/icons/nextLevel.svg" alt="Next Level" width="32" />
+          <img src="@/assets/graphics/icons/nextLevel.svg" alt="Next Level" width="24" />
         </router-link>
       </h1>
 
