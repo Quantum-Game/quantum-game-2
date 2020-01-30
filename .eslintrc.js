106c85662acdb597b85c3462a3727f92c368e52e
Piotr Migdal <pmigdal@gmail.com>
2020-01-30 18:02:28 +0100

    fix(lint) console.log is now fine (lint/build consistency)

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
     '@vue/prettier'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     '@typescript-eslint/interface-name-prefix': ['error', 'always']
   },
