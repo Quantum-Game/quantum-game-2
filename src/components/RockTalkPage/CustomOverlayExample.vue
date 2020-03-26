d5cfdbf9763e3a89c260f3f3b723a08588647e9f
Jakub Jan Strebeyko <strebeyko@gmx.com>
2020-03-26 20:13:21 +0100

    create the RockTalkPage :muscle:

---
+++ src/components/RockTalkPage/CustomOverlayExample.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>This is a custom overlay!</h1>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class CustomOverlayExample extends Vue {}
+</script>
+
+<style></style>
