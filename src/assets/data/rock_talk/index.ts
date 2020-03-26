6a9c3c3ef43c62ce35e70413e5c54aacc6349ecb
Jakub Jan Strebeyko <strebeyko@gmx.com>
2020-03-26 20:12:02 +0100

    Create rock_talk asset file & helper functions/typecasts

---
+++ src/assets/data/rock_talk/index.ts
@@ -0,0 +1,58 @@
+import rockTalkData from './rock_talk.json'
+
+/**
+ * rock talk entry type casts:
+ */
+interface IDialogueLine {
+  content: string
+  color: string
+}
+
+export interface IRockTalk {
+  id: string
+  type: string
+  dialogue: IDialogueLine[]
+}
+
+export const rockTalk: IRockTalk[] = [...rockTalkData]
+
+/**
+ * mapping of level IDs against the overlays coming afterwards
+ */
+export const postLevelOverlayMapping: {
+  [key: number]: string
+} = {
+  1: 'introduction',
+  2: 'custom',
+  3: 'talking'
+}
+
+/**
+ * Used by GamePage. Takes a current level ID and
+ * checks whether there should be an overlay coming afterwards.
+ * @param id an level id, corresponding to the
+ * postLevelOverlayMapping overlay index
+ * @returns an overlay name (string) or null
+ * if there is no overlay to display
+ */
+export const getOverlayNameByLevelId = (id: number): string | null => {
+  return postLevelOverlayMapping[id] || null
+}
+
+/**
+ * Used by InterLevelOverlay to establish rock dialogues.
+ * @param id an id of an overlay
+ * @returns a rocktalk entry or null, if there isn't any
+ * with given ID.
+ */
+export const getRockTalkById = (id: string): IRockTalk | null => {
+  const rtEntry = rockTalk.find((x) => x.id === id)
+  return rtEntry || null
+}
+
+export default {
+  ...rockTalk,
+  postLevelOverlayMapping,
+  getOverlayNameByLevelId,
+  getRockTalkById
+}
