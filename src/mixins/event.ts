9264b4c80e4e56b671399984f40ac6768f557164
Frizi <frizi09@gmail.com>
2020-09-14 17:18:40 +0200

    add ResizeObserved based size change detection

--- src/mixins/event.ts
+++ src/mixins/event.ts
@@ -1,4 +1,4 @@
-import { onMounted, onUnmounted } from 'vue'
+import { onMounted, onUnmounted, Ref, watch, ref } from 'vue'
 
 /**
  * Add an event listener on window for the duration of component lifetime.
@@ -15,4 +15,40 @@ export function useWindowEvent<K extends keyof WindowEventMap>(
   onUnmounted(() => {
     window.removeEventListener(event, handler)
   })
+}
+
+/**
+ * Get DOM node size and keep it up to date.
+ *
+ * # Warning:
+ * Updating DOM node layout based on values derived from their size can introduce
+ * unwanted feedback loops across the script and layout reflow. Avoid doing that.
+ *
+ * @param elementRef Ref obejct the DOM node to observe.
+ * @returns ref containing always up-to-date content DOMRect of the target DOM node.
+ */
+export function useDOMRect(elementRef: Ref<HTMLElement | undefined>): Ref<DOMRectReadOnly> {
+  const sizeRef = ref(new DOMRect())
+  const observer = new ResizeObserver((entries) => {
+    let rect = null
+    for (const entry of entries) {
+      if (entry.target === elementRef.value) {
+        rect = entry.contentRect
+      }
+    }
+    if (rect != null) {
+      sizeRef.value = rect
+    }
+  })
+
+  watch(elementRef, (newNode, oldNode) => {
+    if (oldNode != null) {
+      observer.unobserve(oldNode)
+    }
+    if (newNode != null) {
+      observer.observe(newNode)
+    }
+  })
+
+  return sizeRef
 }
--- src/thirdparty.d.ts
+++ src/thirdparty.d.ts
@@ -6,4 +6,33 @@ declare module 'vue-confetti' {
   export { Confetti }
 }
 declare module 'tone'
-// declare module 'quantum-tensors'
+
+// NOTE: ResizeObserver declaration is temporary, until the builtin typedefs are updated.
+declare class ResizeObserver {
+  constructor(callback: ResizeObserverCallback)
+  disconnect: () => void
+  observe: (target: Element, options?: ResizeObserverObserveOptions) => void
+  unobserve: (target: Element) => void
+}
+
+interface ResizeObserverObserveOptions {
+  box?: 'content-box' | 'border-box'
+}
+
+type ResizeObserverCallback = (entries: ResizeObserverEntry[], observer: ResizeObserver) => void
+
+interface ResizeObserverEntry {
+  readonly borderBoxSize: ResizeObserverEntryBoxSize
+  readonly contentBoxSize: ResizeObserverEntryBoxSize
+  readonly contentRect: DOMRectReadOnly
+  readonly target: Element
+}
+
+interface ResizeObserverEntryBoxSize {
+  blockSize: number
+  inlineSize: number
+}
+
+interface Window {
+  ResizeObserver: typeof ResizeObserver
+}
