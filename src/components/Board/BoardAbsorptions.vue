a0521f5dbb30eb340a3c7794af0d1292aee28031
Frizi <frizi09@gmail.com>
2020-10-19 14:58:34 +0200

    add circular absorption display under a feature flag

--- src/components/Board/BoardAbsorptions.vue
+++ src/components/Board/BoardAbsorptions.vue
@@ -12,32 +12,30 @@
   </text>
   <!-- GOALS -->
   <g v-for="[coord, { threshold, value }] in goalPercents" :key="`goal-${coord.x}-${coord.y}`">
-    <rect fill="#4F2F7B" :x="coord.x * 64 + 2" :y="coord.y * 64 - 8" :width="64 - 4" height="5" />
-    <rect
-      fill="url(#progress-gradient)"
-      :x="coord.x * 64 + 2"
-      :y="coord.y * 64 - 8"
-      :width="(64 - 4) * value"
-      height="5"
-      class="progress-bar"
-    />
-
-    <rect
-      stroke="white"
-      :x="coord.x * 64 + 2 + (64 - 4) * threshold"
-      :y="coord.y * 64 - 9"
-      :width="0.01"
-      height="7"
-    />
-    <text
-      :x="(coord.x + 0.5) * 64"
-      :y="coord.y * 64 - 12"
-      text-anchor="middle"
-      class="goal-probability"
-      :class="{ 'goal-met': value >= threshold }"
-    >
-      {{ (value * 100).toFixed(1) }}%
-    </text>
+    <template v-if="$flags.circleAbsorptions">
+      <AbsorptionCircle :coord="coord" :threshold="threshold" :value="value" />
+      <text
+        :x="(coord.x + 0.5) * 64"
+        :y="coord.y * 64 - 2"
+        text-anchor="middle"
+        class="goal-probability"
+        :class="{ 'goal-met': value >= threshold }"
+      >
+        {{ (value * 100).toFixed(1) }}%
+      </text>
+    </template>
+    <template v-else>
+      <AbsorptionBar :coord="coord" :threshold="threshold" :value="value" />
+      <text
+        :x="(coord.x + 0.5) * 64"
+        :y="coord.y * 64 - 12"
+        text-anchor="middle"
+        class="goal-probability"
+        :class="{ 'goal-met': value >= threshold }"
+      >
+        {{ (value * 100).toFixed(1) }}%
+      </text>
+    </template>
   </g>
 </template>
 
@@ -45,8 +43,13 @@
 import { Coord } from '@/engine/model'
 import { iFilter, mapEntries } from '@/itertools'
 import { computed, defineComponent, PropType } from 'vue'
-
+import AbsorptionCircle from './AbsorptionCircle.vue'
+import AbsorptionBar from './AbsorptionBar.vue'
 export default defineComponent({
+  components: {
+    AbsorptionCircle,
+    AbsorptionBar,
+  },
   props: {
     absorptions: { type: Map as PropType<Map<Coord, number>>, required: true },
     goals: { type: Map as PropType<Map<Coord, number>>, required: true },
--- src/components/EncyclopediaPage/EncyclopediaBoard.vue
+++ src/components/EncyclopediaPage/EncyclopediaBoard.vue
@@ -2,7 +2,7 @@
   <div v-if="gameCtl.level" class="container" layout="column center u1">
     <Board
       class="board"
-      :absorptions="gameCtl.sim.absorptions"
+      :absorptions="totalAbsorptions"
       :board="gameCtl.level.board"
       :laserParticles="laserParticles"
       :particles="playheadCtl.interpolatedParticles"
@@ -61,6 +61,11 @@ export default defineComponent({
       return props.vector?.dimensions.find((d) => d.name === 'polarization')?.coordString ?? 'HV'
     })
 
+    const totalAbsorptions = computed(() => {
+      let absorptions = gameCtl.sim?.upToFrameAbsorptions ?? []
+      return absorptions[absorptions.length - 1] ?? new Map()
+    })
+
     function updateRotation(coord: Coord) {
       gameCtl.rotateCcw(coord)
       const piece = gameCtl.level?.board.pieces.get(coord)
@@ -69,7 +74,7 @@ export default defineComponent({
       }
     }
 
-    return { gameCtl, playheadCtl, laserParticles, polBasis, updateRotation }
+    return { gameCtl, playheadCtl, laserParticles, polBasis, updateRotation, totalAbsorptions }
   },
 })
 </script>
